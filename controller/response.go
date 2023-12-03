1029626b41b43f3a1495172a3e2340dc235dfda0
emersonfbarros <emersonfb99@gmail.com>
2023-12-02 22:25:28 -0300

    refactor: improves responses functions

--- controller/response.go
+++ controller/response.go
@@ -6,9 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message": msg,
+	})
+}
+
 func sendSuccess(ctx *gin.Context, data interface{}) {
 	ctx.Header("Content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"data": data,
-	})
+	ctx.JSON(http.StatusOK, data)
 }
