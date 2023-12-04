e341ffdc2f33c928ca6d88e194b88511ccb3bc6c
emersonfbarros <emersonfb99@gmail.com>
2023-12-04 10:40:06 -0300

    feat: improves router initialization method

--- router/router.go
+++ router/router.go
@@ -1,19 +1,25 @@
 package router
 
 import (
-	"log"
+	"os"
 
+	"github.com/emersonfbarros/backend-challenge-klever/config"
 	"github.com/gin-gonic/gin"
 )
 
+var logger *config.Logger
+
 func Initialize(port string) {
+	logger = config.GetLogger("router")
 	// starts router with gin defaults
 	router := gin.Default()
 
 	initRoutes(router)
 
 	// runs server
+	logger.Infof("Server running on port %s", port)
 	if err := router.Run(port); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
+		logger.Errorf("Failed to run server: %v", err)
+		os.Exit(1)
 	}
 }
