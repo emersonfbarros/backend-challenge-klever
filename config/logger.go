ed2f9d006bc327a9f23913e0073f9e16238d6971
emersonfbarros <emersonfb99@gmail.com>
2023-12-04 10:24:51 -0300

    feat: adds info custom logger

--- config/logger.go
+++ config/logger.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Logger struct {
+	info   *log.Logger
 	err    *log.Logger
 	writer io.Writer
 }
@@ -16,9 +17,14 @@ func NewLogger(p string) *Logger {
 	logger := log.New(writer, p, log.Ldate|log.Ltime)
 
 	return &Logger{
+		info:   log.New(writer, "INFO: ", logger.Flags()),
 		err:    log.New(writer, "ERROR: ", logger.Flags()),
 		writer: writer,
 	}
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.info.Printf(format, v...)
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
