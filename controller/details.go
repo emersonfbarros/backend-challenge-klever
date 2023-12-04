7e0a084a7f0d0356aeaf1129eb37d48a8afc5913
emersonfbarros <emersonfb99@gmail.com>
2023-12-04 15:19:16 -0300

    feate: creates details service

--- controller/details.go
+++ controller/details.go
@@ -1,8 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/emersonfbarros/backend-challenge-klever/service"
+	"github.com/gin-gonic/gin"
+)
 
 func Details(context *gin.Context) {
 	address := context.Param("address")
+	service.Details(address)
 	sendSuccess(context, address)
 }
---
+++ service/details.go
@@ -0,0 +1,5 @@
+package service
+
+func Details(address string) {
+	logger.Infof("RECEIVED ADDRESS: %s", address)
+}
