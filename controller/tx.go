8f7cd65e433c6dced6480c5ef27440b563ecebc7
emersonfbarros <emersonfb99@gmail.com>
2023-12-05 11:00:45 -0300

    feat: implements tx controller

--- controller/tx.go
+++ controller/tx.go
@@ -1,12 +1,18 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/emersonfbarros/backend-challenge-klever/service"
 	"github.com/gin-gonic/gin"
 )
 
 func Tx(context *gin.Context) {
 	tx := context.Param("tx")
-	service.Tx(tx)
-	sendSuccess(context, tx)
+	transaction, err := service.Tx(tx)
+	if err != nil {
+		sendError(context, http.StatusBadGateway, err.Error())
+	}
+
+	sendSuccess(context, *transaction)
 }
