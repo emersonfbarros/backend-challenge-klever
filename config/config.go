9b4edea8fc2c8b738e43be2eddcd157ec7ae0896
emersonfbarros <emersonfb99@gmail.com>
2023-12-05 13:51:33 -0300

    feat: creates health service

--- config/config.go
+++ config/config.go
@@ -1,10 +1,16 @@
 package config
 
 import (
+	"time"
+
 	"github.com/joho/godotenv"
 )
 
+var AppStartTime time.Time
+
 func Init() error {
+	AppStartTime = time.Now()
+
 	err := godotenv.Load()
 
 	if err != nil {
