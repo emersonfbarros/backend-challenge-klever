48d28bd9b89cfaaad86994fbec0241609fbacbab
emersonfbarros <emersonfb99@gmail.com>
2023-12-05 10:47:19 -0300

    feat: adds logic to tx service

--- service/tx.go
+++ service/tx.go
@@ -1,5 +1,49 @@
 package service
 
-func Tx(tx string) {
-	logger.Infof("RECEIVED TX: %s", tx)
+import (
+	"fmt"
+
+	"github.com/emersonfbarros/backend-challenge-klever/model"
+)
+
+type Transaction struct {
+	Addresses []Address `json:"addresses"`
+	Block     int       `json:"block"`
+	TxID      string    `json:"txID"`
+}
+
+type Address struct {
+	Address string `json:"address"`
+	Value   string `json:"value"`
+}
+
+func Tx(txId string) (*Transaction, error) {
+	txRef, err := model.GetTx(txId)
+	if err != nil {
+		logger.Errorf("failed to unmarshal api response %v", err.Error())
+		return nil, fmt.Errorf("failed to request external resouce")
+	}
+
+	extTx := *txRef
+
+	transaction := Transaction{
+		Block: extTx.BlockHeight,
+		TxID:  txId,
+	}
+
+	for _, vin := range extTx.Vin {
+		transaction.Addresses = append(transaction.Addresses, Address{
+			Address: vin.Addresses[0],
+			Value:   vin.Value,
+		})
+	}
+
+	for _, vout := range extTx.Vout {
+		transaction.Addresses = append(transaction.Addresses, Address{
+			Address: vout.Addresses[0],
+			Value:   vout.Value,
+		})
+	}
+
+	return &transaction, nil
 }
