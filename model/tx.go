40a0390c8349f6bac876aeb28d0481be06ef2e1b
emersonfbarros <emersonfb99@gmail.com>
2023-12-05 10:47:08 -0300

    feat: creates tx model

---
+++ model/tx.go
@@ -0,0 +1,33 @@
+package model
+
+import "encoding/json"
+
+type vin struct {
+	Addresses []string `json:"addresses"`
+	Value     string   `json:"value"`
+}
+
+type vout struct {
+	Value     string   `json:"value"`
+	Addresses []string `json:"addresses"`
+}
+
+type ExtTx struct {
+	Vin         []vin  `json:"vin"`
+	Vout        []vout `json:"vout"`
+	BlockHeight int    `json:"blockHeight"`
+}
+
+func GetTx(txId string) (*ExtTx, error) {
+	body, err := Fetch("tx", txId)
+	if err != nil {
+		return nil, err
+	}
+
+	var extTx ExtTx
+	if err := json.Unmarshal(body, &extTx); err != nil {
+		return nil, err
+	}
+
+	return &extTx, nil
+}
