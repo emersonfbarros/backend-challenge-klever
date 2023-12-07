31fb487f20bee2bcd0b6d5a41fe918b9f7f511d8
emersonfbarros <emersonfb99@gmail.com>
2023-12-07 18:54:44 -0300

    chore: removes unecessary print

--- controller/tx.go
+++ controller/tx.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +10,6 @@ func Tx(context *gin.Context) {
 	tx := context.Param("tx")
 	transaction, err := services.Tx(models, tx)
 	if err != nil {
-		fmt.Printf("\n\nENTROU\n\n")
 		resSender.sendError(context, http.StatusBadGateway, err.Error())
 		return
 	}
