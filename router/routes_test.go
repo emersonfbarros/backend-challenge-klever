7fb5ba336b39774d5cccb5b33171118bf2a62bfe
emersonfbarros <emersonfb99@gmail.com>
2023-12-09 20:04:43 -0300

    test: fixes and improves integration tests

--- router/routes_test.go
+++ router/routes_test.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -12,7 +13,7 @@ import (
 )
 
 func TestRoutesIntegration(t *testing.T) {
-	utxoRes := `[
+	utxoResSuccess := `[
 		{
 			"txid": "1c16ffaad93464a35af0501b95274fe08e2f68beeadc1599cda14f2fb612f1b6",
 			"value": "450460",
@@ -30,14 +31,14 @@ func TestRoutesIntegration(t *testing.T) {
 		}
 	]`
 
-	addressRes := `{
+	addressResSuccess := `{
 		"balance": "17454817",
 		"totalReceived": "193498135",
 		"totalSent": "176043318",
 		"txs": 647
 	}`
 
-	txRes := `{
+	txResSuccess := `{
 		"vin": [
 			{
 				"addresses": ["bc1qyzxdu4px4jy8gwhcj82zpv7qzhvc0fvumgnh0r"],
@@ -63,49 +64,64 @@ func TestRoutesIntegration(t *testing.T) {
 
 	address := "19SH3YrkrpWXKtCoMXWfoVpmUF1ZHAi24n"
 	tx := "3654d26660dcc05d4cfb25a1641a1e61f06dfeb38ee2279bdb049d018f1830ab"
+	invalidAddress := "1nv4lid_4ddre55"
+	// invalidTx := "1nv4lid_7x"
 
-	notFoundRes := `{"message":"failed to request external resource"}`
+	serverErrorMsg := `{"error":"Whatever not found"}`
+	// badGatewayMds := `{"message":"Failed to request external resource"}`
+	notFoundAddress := fmt.Sprintf(`{"message":"Address %s not found"}`, invalidAddress)
+	// notFoundTx := fmt.Sprintf("Transaction %s not found", invalidTx)
 
 	tests := []struct {
-		name         string
-		tx           string
-		address      string
-		route        string
-		expectedBody string
-		expectedCode int
+		name            string
+		tx              string
+		address         string
+		route           string
+		expectedBody    string
+		expectedCode    int
+		utxoRouteRes    string
+		txRouteRes      string
+		addressRouteRes string
 	}{
 		{
 			name:         "Test balance route on success",
 			address:      address,
 			route:        "balance/",
+			utxoRouteRes: utxoResSuccess,
 			expectedBody: `{"confirmed":"705425","unconfirmed":"0"}`,
 			expectedCode: http.StatusOK,
 		},
 		{
-			name:         "Test balance route on error",
-			address:      "1v4lid_4ddre55",
+			name:         "Test balance route on not found",
+			address:      invalidAddress,
 			route:        "balance/",
-			expectedBody: notFoundRes,
-			expectedCode: http.StatusBadGateway,
+			utxoRouteRes: serverErrorMsg,
+			expectedBody: notFoundAddress,
+			expectedCode: http.StatusNotFound,
 		},
 		{
-			name:         "Test details route on success",
-			address:      address,
-			route:        "details/",
-			expectedBody: `{"address":"19SH3YrkrpWXKtCoMXWfoVpmUF1ZHAi24n","balance":"17454817","totalTx":647,"balanceCalc":{"confirmed":"705425","unconfirmed":"0"},"total":{"sent":"176043318","received":"193498135"}}`,
-			expectedCode: http.StatusOK,
+			name:            "Test details route on success",
+			address:         address,
+			route:           "details/",
+			utxoRouteRes:    utxoResSuccess,
+			addressRouteRes: addressResSuccess,
+			expectedBody:    `{"address":"19SH3YrkrpWXKtCoMXWfoVpmUF1ZHAi24n","balance":"17454817","totalTx":647,"balanceCalc":{"confirmed":"705425","unconfirmed":"0"},"total":{"sent":"176043318","received":"193498135"}}`,
+			expectedCode:    http.StatusOK,
 		},
 		{
-			name:         "Test details route on error",
-			address:      "1v4lid_4ddre55",
-			route:        "details/",
-			expectedBody: notFoundRes,
-			expectedCode: http.StatusBadGateway,
+			name:            "Test details route on error",
+			address:         invalidAddress,
+			route:           "details/",
+			utxoRouteRes:    serverErrorMsg,
+			addressRouteRes: serverErrorMsg,
+			expectedBody:    notFoundAddress,
+			expectedCode:    http.StatusNotFound,
 		},
 		{
 			name:         "Test tx route on success",
 			tx:           tx,
 			route:        "tx/",
+			txRouteRes:   txResSuccess,
 			expectedBody: `{"addresses":[{"address":"bc1qyzxdu4px4jy8gwhcj82zpv7qzhvc0fvumgnh0r","value":"484817655"},{"address":"36iYTpBFVZPbcyUs8pj3BtutZXzN6HPNA6","value":"623579"},{"address":"bc1qe29ydjtwyjdmffxg4qwtd5wfwzdxvnap989glq","value":"3283266"},{"address":"bc1qanhueax8r4cn52r38f2h727mmgg6hm3xjlwd0x","value":"90311"}],"block":675674,"txID":"3654d26660dcc05d4cfb25a1641a1e61f06dfeb38ee2279bdb049d018f1830ab"}`,
 			expectedCode: http.StatusOK,
 		},
@@ -116,18 +132,14 @@ func TestRoutesIntegration(t *testing.T) {
 			// http server to mock exeternal api
 			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				switch r.URL.Path {
-				case "/utxo/" + address:
-					w.WriteHeader(http.StatusOK)
-					w.Write([]byte(utxoRes))
-				case "/address/" + address:
-					w.WriteHeader(http.StatusOK)
-					w.Write([]byte(addressRes))
-				case "/tx/" + tx:
-					w.WriteHeader(http.StatusOK)
-					w.Write([]byte(txRes))
+				case "/utxo/" + tt.address:
+					w.Write([]byte(tt.utxoRouteRes))
+				case "/address/" + tt.address:
+					w.Write([]byte(tt.addressRouteRes))
+				case "/tx/" + tt.tx:
+					w.Write([]byte(tt.txRouteRes))
 				default:
-					w.WriteHeader(http.StatusNotFound)
-					w.Write([]byte(notFoundRes))
+					w.Write([]byte(serverErrorMsg))
 				}
 			}))
 			// close test server when test finishes
