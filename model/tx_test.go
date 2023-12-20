378b5f5fbadf4f56edea99e806b16b6ca7119e2b
emersonfbarros <emersonfb99@gmail.com>
2023-12-20 20:22:37 -0300

    test: updates tx_test

--- model/tx_test.go
+++ model/tx_test.go
@@ -7,9 +7,23 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestGetTxSuccess(t *testing.T) {
+type MockMemCache struct {
+	mock.Mock
+}
+
+func (mc *MockMemCache) Set(key string, value interface{}) {
+	mc.Called(key, value)
+}
+
+func (mc *MockMemCache) Get(key string) (interface{}, bool) {
+	args := mc.Called(key)
+	return args.Get(0), args.Bool(1)
+}
+
+func TestGetTxSuccessWithoutCache(t *testing.T) {
 	expectedTx := ExtTx{
 		Vin:         []Vin{{Addresses: []string{"address1"}, Value: "100"}},
 		Vout:        []Vout{{Value: "100", Addresses: []string{"address2"}}},
@@ -19,10 +33,15 @@ func TestGetTxSuccess(t *testing.T) {
 
 	// initialize mock, declared in model/address_test
 	mockFetcher := new(MockFetcher)
+	mockCacher := new(MockMemCache)
 
 	mockFetcher.On("Fetch", "tx", "tx123").Return(expectedTxBytes, nil)
+	mockCacher.On("Get", "tx123").Return(nil, false)
+	mockCacher.On("Set", "tx123", expectedTx).Return()
 
-	models := &Models{}
+	models := &Models{
+		cacher: mockCacher,
+	}
 
 	resultTx, err, httpCode := models.GetTx(mockFetcher, "tx123")
 
@@ -32,6 +51,34 @@ func TestGetTxSuccess(t *testing.T) {
 	assert.Equal(t, 0, httpCode)
 
 	mockFetcher.AssertExpectations(t)
+	mockCacher.AssertExpectations(t)
+}
+
+func TestGetTxSuccessWithCache(t *testing.T) {
+	expectedTx := ExtTx{
+		Vin:         []Vin{{Addresses: []string{"address1"}, Value: "100"}},
+		Vout:        []Vout{{Value: "100", Addresses: []string{"address2"}}},
+		BlockHeight: 123,
+	}
+
+	// initialize mock, declared in model/address_test
+	mockFetcher := new(MockFetcher)
+	mockCacher := new(MockMemCache)
+
+	mockCacher.On("Get", "tx123").Return(expectedTx, true)
+
+	models := &Models{
+		cacher: mockCacher,
+	}
+
+	resultTx, err, httpCode := models.GetTx(mockFetcher, "tx123")
+
+	// Asserts
+	assert.NoError(t, err)
+	assert.Equal(t, &expectedTx, resultTx)
+	assert.Equal(t, 0, httpCode)
+
+	mockCacher.AssertExpectations(t)
 }
 
 func TestGetTxErrorFetch(t *testing.T) {
@@ -39,10 +86,14 @@ func TestGetTxErrorFetch(t *testing.T) {
 
 	// initialize mock, declared in model/address_test
 	mockFetcher := new(MockFetcher)
+	mockCacher := new(MockMemCache)
 
 	mockFetcher.On("Fetch", "tx", "tx123").Return(expectedTxBytes, errors.New("fetch error"))
+	mockCacher.On("Get", "tx123").Return(nil, false)
 
-	models := &Models{}
+	models := &Models{
+		cacher: mockCacher,
+	}
 
 	resultTx, err, httpCode := models.GetTx(mockFetcher, "tx123")
 
@@ -52,6 +103,7 @@ func TestGetTxErrorFetch(t *testing.T) {
 	assert.Equal(t, http.StatusBadGateway, httpCode)
 
 	mockFetcher.AssertExpectations(t)
+	mockCacher.AssertExpectations(t)
 }
 
 func TestGetTxErrorUnmarshal(t *testing.T) {
@@ -60,10 +112,14 @@ func TestGetTxErrorUnmarshal(t *testing.T) {
 
 	// initialize mock, declared in model/address_test
 	mockFetcher := new(MockFetcher)
+	mockCacher := new(MockMemCache)
 
 	mockFetcher.On("Fetch", "tx", "tx123").Return(expectedTxBytes, nil)
+	mockCacher.On("Get", "tx123").Return(nil, false)
 
-	models := &Models{}
+	models := &Models{
+		cacher: mockCacher,
+	}
 
 	resultTx, err, httpCode := models.GetTx(mockFetcher, "tx123")
 
@@ -73,4 +129,5 @@ func TestGetTxErrorUnmarshal(t *testing.T) {
 	assert.Equal(t, http.StatusInternalServerError, httpCode)
 
 	mockFetcher.AssertExpectations(t)
+	mockCacher.AssertExpectations(t)
 }
